20564673f3299cf20d2dc24716ab7029d0d0c1ff
Roberto Plancarte <perezplancarter@gmail.com>
2025-07-15 16:06:22 -0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -6,32 +6,32 @@ export const repos = [
     url: "https://github.com/Choilolox/Kotlin-Files"
   },
   {
-    name: "SISTEMA-INVENTARIO-JAVA",
+    name: "JAVA INVENTORY SYSTEM",
     description: "Point of sale in Java developed from scratch",
     image: "/javaimg.jpg",
     url: "https://github.com/Choilolox/SistemaInventario"
   },
   {
-    name: "SISTEMA-INVENTARIO-KOTLIN-MIGRACION",
+    name: "SYSTEM INVENTORY KOTLIN MIGRATION",
     description: "Migrating a Java point-of-sale project to Kotlin",
     image: "/kotlinimg.png",
     url: "https://github.com/Choilolox/SistemaInventarioKotlin/tree/main/main/kotlin/com/puntodeventa"
   },
   {
-    name: "APP-CHECADOR-REACT",
+    name: "REACT CHECKER APP",
     description: "Input and output application developed in react",
     image: "/reactimg.png",
     url: "https://github.com/Choilolox/ControlAsistencias"
   },
   {
-    name: "MUSICA-ANIMO",
+    name: "MUSIC MOOD IA",
     description: "Music Recommender according to your mood",
     image: "/reactimg.png",
-    url: "https://github.com/Choilolox/MUSICA-ANIMO"
+    url: "https://github.com/Choilolox/EstadoAnimo"
   },
 
    {
-    name: "SOPORTE-TI",
+    name: "SUPPORT-TI",
     description: "It support app",
     image: "/kotlinimg.png",
     url: "https://github.com/Choilolox/AppSoporte"
